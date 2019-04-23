acd45ddd2b2bdb4aeb8b1a97c7015b15321859d7
lishujun <lishujun@sirbox.cn>
2019-04-23 07:49:48 +0000

    modify

--- src/sirbox-dev/start-build.sh
+++ src/sirbox-dev/start-build.sh
@@ -1,8 +1,20 @@
 #!/bin/bash
 
 # download rpm
-wget http://log4c.com/centos/jdk-8u201-linux-x64.rpm
-wget http://log4c.com/centos/mysql-community-release-el7-5.noarch.rpm
-wget http://log4c.com/centos/nginx-release-centos-7-0.el7.ngx.noarch.rpm
 
+download(){
+    if [ ! -f $1 ]; then
+        echo "download $1..."
+        wget http://log4c.com/centos/$1
+    else
+        echo "$1 is exists"
+    fi
+}
+
+
+download jdk-8u201-linux-x64.rpm
+download mysql-community-release-el7-5.noarch.rpm
+download nginx-release-centos-7-0.el7.ngx.noarch.rpm
+
+echo "build image..."
 docker build -t sirbox/dev:0.1 . 
--- src/sirbox-dev/start-container.sh
+++ src/sirbox-dev/start-container.sh
@@ -1,4 +1,13 @@
 #!/bin/bash
 
+if [ $# -le 0 ]; then
+    echo "$0 s01 8080"
+    exit
+fi
+
+echo "start container"
 docker run --privileged --name $1 -itd -p $2:80 sirbox/dev:0.1
-docker exec -it $1 mysqladmin -uroot password "sirbox.cn"
+
+sleep 20
+echo "set password"
+docker exec -d $1 /bin/bash -c 'mysqladmin -uroot password sirbox.cn'
